5d758739188c341979f31754c0827e30e81ff3c3
gnvtr <gustavo.cesargoncalves93@gmail.com>
2023-04-26 19:30:11 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String nome = "Gustavo";
+		int idade = 29;  
+		double renda = 2021.0; 
+		
+		System.out.printf("%s tem %d anos de idade e ganha R$ %.2f%n", nome, idade, renda);
+	}
+
+}
